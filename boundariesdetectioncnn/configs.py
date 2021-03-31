ca392d87c6b88989e355bc0ece877c04b58d9249
carlosholivan <carlosholivan@gmail.com>
2021-03-31 11:11:38 +0200

    add pretrained weights

--- boundariesdetectioncnn/configs.py
+++ boundariesdetectioncnn/configs.py
@@ -25,37 +25,36 @@ class PathsConfig:
     MELS_VAL_PATH                           =       "../Inputs/VALIDATION/np MLS/"
     MELS_TEST_PATH                          =       "../Inputs/TEST/np MLS/"
    
-    SSLM_COS_CHROMAS_SMALL_TRAIN_PATH       =       "../Inputs/TRAIN/np SSLM from Chromas cosine 2pool3/"
-    SSLM_COS_CHROMAS_SMALL_VAL_PATH         =       "../Inputs/VALIDATION/np SSLM from Chromas cosine 2pool3/"
-    SSLM_COS_CHROMAS_SMALL_TEST_PATH        =       "../Inputs/TEST/np SSLM from Chromas cosine 2pool3/"
+    SSLM_COS_CHROMAS_LARGE_TRAIN_PATH       =       "../Inputs/TRAIN/np SSLM from Chromas cosine 2pool3/"
+    SSLM_COS_CHROMAS_LARGE_VAL_PATH         =       "../Inputs/VALIDATION/np SSLM from Chromas cosine 2pool3/"
+    SSLM_COS_CHROMAS_LARGE_TEST_PATH        =       "../Inputs/TEST/np SSLM from Chromas cosine 2pool3/"
 
-    SSLM_COS_CHROMAS_LARGE_TRAIN_PATH       =       "../Inputs/TRAIN/np SSLM from Chromas cosine/"
-    SSLM_COS_CHROMAS_LARGE_VAL_PATH         =       "../Inputs/VALIDATION/np SSLM from Chromas cosine/"
-    SSLM_COS_CHROMAS_LARGE_TEST_PATH        =       "../Inputs/TEST/np SSLM from Chromas cosine/"
+    SSLM_COS_CHROMAS_SMALL_TRAIN_PATH       =       "../Inputs/TRAIN/np SSLM from Chromas cosine/"
+    SSLM_COS_CHROMAS_SMALL_VAL_PATH         =       "../Inputs/VALIDATION/np SSLM from Chromas cosine/"
+    SSLM_COS_CHROMAS_SMALL_TEST_PATH        =       "../Inputs/TEST/np SSLM from Chromas cosine/"
 
-    SSLM_EUCL_CHROMAS_SMALL_TRAIN_PATH      =       "../Inputs/TRAIN/np SSLM from Chromas euclidean 2pool3/"
-    SSLM_EUCL_CHROMAS_SMALL_VAL_PATH        =       "../Inputs/VALIDATION/np SSLM from Chromas euclidean 2pool3/"
-    SSLM_EUCL_CHROMAS_SMALL_TEST_PATH       =       "../Inputs/TEST/np SSLM from Chromas euclidean 2pool3/"
+    SSLM_EUCL_CHROMAS_LARGE_TRAIN_PATH      =       "../Inputs/TRAIN/np SSLM from Chromas euclidean 2pool3/"
+    SSLM_EUCL_CHROMAS_LARGE_VAL_PATH        =       "../Inputs/VALIDATION/np SSLM from Chromas euclidean 2pool3/"
+    SSLM_EUCL_CHROMAS_LARGE_TEST_PATH       =       "../Inputs/TEST/np SSLM from Chromas euclidean 2pool3/"
 
-    SSLM_EUCL_CHROMAS_LARGE_TRAIN_PATH      =       "../Inputs/TRAIN/np SSLM from Chromas euclidean/"
-    SSLM_EUCL_CHROMAS_LARGE_VAL_PATH        =       "../Inputs/VALIDATION/np SSLM from Chromas euclidean/"
-    SSLM_EUCL_CHROMAS_LARGE_TEST_PATH       =       "../Inputs/TEST/np SSLM from Chromas euclidea/n"
+    SSLM_EUCL_CHROMAS_SMALL_TRAIN_PATH      =       "../Inputs/TRAIN/np SSLM from Chromas euclidean/"
+    SSLM_EUCL_CHROMAS_SMALL_VAL_PATH        =       "../Inputs/VALIDATION/np SSLM from Chromas euclidean/"
+    SSLM_EUCL_CHROMAS_SMALL_TEST_PATH       =       "../Inputs/TEST/np SSLM from Chromas euclidea/n"
 
+    SSLM_COS_MFCCS_LARGE_TRAIN_PATH         =       "../Inputs/TRAIN/np SSLM from MFCCs cosine 2pool3/"
+    SSLM_COS_MFCCS_LARGE_VAL_PATH           =       "../Inputs/VALIDATION/np SSLM from MFCCs cosine 2pool3/"
+    SSLM_COS_MFCCS_LARGE_TEST_PATH          =       "../Inputs/TEST/np SSLM from MFCCs cosine 2pool3/"
 
-    SSLM_COS_MFCCS_SMALL_TRAIN_PATH         =       "../Inputs/TRAIN/np SSLM from MFCCs cosine 2pool3/"
-    SSLM_COS_MFCCS_SMALL_VAL_PATH           =       "../Inputs/VALIDATION/np SSLM from MFCCs cosine 2pool3/"
-    SSLM_COS_MFCCS_SMALL_TEST_PATH          =       "../Inputs/TEST/np SSLM from MFCCs cosine 2pool3/"
+    SSLM_COS_MFCCS_SMALL_TRAIN_PATH         =       "../Inputs/TRAIN/np SSLM from MFCCs cosine/"
+    SSLM_COS_MFCCS_SMALL_VAL_PATH           =       "../Inputs/VALIDATION/np SSLM from MFCCs cosine/"
+    SSLM_COS_MFCCS_SMALL_TEST_PATH          =       "../Inputs/TEST/np SSLM from MFCCs cosine/"
 
-    SSLM_COS_MFCCS_LARGE_TRAIN_PATH         =       "../Inputs/TRAIN/np SSLM from MFCCs cosine/"
-    SSLM_COS_MFCCS_LARGE_VAL_PATH           =       "../Inputs/VALIDATION/np SSLM from MFCCs cosine/"
-    SSLM_COS_MFCCS_LARGE_TEST_PATH          =       "../Inputs/TEST/np SSLM from MFCCs cosine/"
+    SSLM_EUCL_MFCCS_LARGE_TRAIN_PATH        =       "../Inputs/TRAIN/np SSLM from MFCCs euclidean 2pool3/"
+    SSLM_EUCL_MFCCS_LARGE_VAL_PATH          =       "../Inputs/VALIDATION/np SSLM from MFCCs euclidean 2pool3/"
+    SSLM_EUCL_MFCCS_LARGE_TEST_PATH         =       "../Inputs/TEST/np SSLM from MFCCs euclidean 2pool3/"
 
-    SSLM_EUCL_MFCCS_SMALL_TRAIN_PATH        =       "../Inputs/TRAIN/np SSLM from MFCCs euclidean 2pool3/"
-    SSLM_EUCL_MFCCS_SMALL_VAL_PATH          =       "../Inputs/VALIDATION/np SSLM from MFCCs euclidean 2pool3/"
-    SSLM_EUCL_MFCCS_SMALL_TEST_PATH         =       "../Inputs/TEST/np SSLM from MFCCs euclidean 2pool3/"
-
-    SSLM_EUCL_MFCCS_LARGE_TRAIN_PATH        =       "../Inputs/TRAIN/np SSLM from MFCCs euclidean/"
-    SSLM_EUCL_MFCCS_LARGE_VAL_PATH          =       "../Inputs/VALIDATION/np SSLM from MFCCs euclidean/"
-    SSLM_EUCL_MFCCS_LARGE_TEST_PATH         =       "../Inputs/TEST/np SSLM from MFCCs euclidean/"
+    SSLM_EUCL_MFCCS_SMALL_TRAIN_PATH        =       "../Inputs/TRAIN/np SSLM from MFCCs euclidean/"
+    SSLM_EUCL_MFCCS_SMALL_VAL_PATH          =       "../Inputs/VALIDATION/np SSLM from MFCCs euclidean/"
+    SSLM_EUCL_MFCCS_SMALL_TEST_PATH         =       "../Inputs/TEST/np SSLM from MFCCs euclidean/"
 
     LABELS_PATH                             =       Path("E:\\UNIVERSIDAD\\MÁSTER INGENIERÍA INDUSTRIAL\\TFM\\Database\\salami-data-public\\annotations\\")
